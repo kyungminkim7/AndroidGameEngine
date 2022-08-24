d60944bdf7f98ea1630ebe994c19647cd3cccdb4
Your Name <you@example.com>
2022-08-24 18:52:50 -0500

    Modified GetGLM.cmake FetchContent variable name to standardized lowercase

--- app/src/main/cpp/cmake/GetGLM.cmake
+++ app/src/main/cpp/cmake/GetGLM.cmake
@@ -1,8 +1,8 @@
 include(FetchContent)
 
-FetchContent_Declare(GLM
+FetchContent_Declare(glm
     GIT_REPOSITORY https://github.com/g-truc/glm.git
     GIT_TAG 0.9.9.8
 )
 
-FetchContent_MakeAvailable(GLM)
+FetchContent_MakeAvailable(glm)
