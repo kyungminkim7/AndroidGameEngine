d647c7cfe883fa3164ea0217fc7418eedb54c516
Your Name <you@example.com>
2022-08-27 14:59:41 -0500

    Fixed assimp options that were causing the game .so files not to be correctly found by dlopen

--- app/src/main/cpp/cmake/GetAssimp.cmake
+++ app/src/main/cpp/cmake/GetAssimp.cmake
@@ -8,11 +8,10 @@ FetchContent_Declare(assimp
 )
 
 OPTION(BUILD_SHARED_LIBS "Build package with shared libraries." OFF)
-OPTION(ASSIMP_ANDROID_JNIIOSYSTEM "Android JNI IOSystem support is active" ON)
-OPTION(ASSIMP_NO_EXPORT "Disable Assimp's export functionality." ON)
 OPTION(ASSIMP_BUILD_ASSIMP_TOOLS "If the supplementary tools for Assimp are built in addition to the library." OFF)
 OPTION(ASSIMP_BUILD_TESTS "If the test suite for Assimp is built in addition to the library." OFF)
 OPTION(ASSIMP_INSTALL "Disable this if you want to use assimp as a submodule." OFF)
+OPTION(ASSIMP_INJECT_DEBUG_POSTFIX "Inject debug postfix in .a/.so/.dll lib names" OFF)
 OPTION(ASSIMP_IGNORE_GIT_HASH "Don't call git to get the hash." ON)
 
 FetchContent_MakeAvailable(assimp)
